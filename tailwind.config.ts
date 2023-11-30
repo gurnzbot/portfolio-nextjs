70449189e5598cb8066b046df8de7cb3e424f6a2
Erik Gurney <erik.gurney@hotmail.com>
2023-11-29 22:25:59 -0500

    refactor(hoempage): minify background image

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
 import type { Config } from 'tailwindcss'
 
-
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -9,17 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      // backgroundImage: {
-      //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-      //   'gradient-conic':
-      //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      // },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
         inconsolata: ['Inconsolata']
       },
       backgroundImage: {
-        intro: "url('/images/homepage-bg.png')"
+        intro: "url('/images/homepage-bg-min.png')"
       },
       animation: {
         cursor: "cursor infinite 800ms"
