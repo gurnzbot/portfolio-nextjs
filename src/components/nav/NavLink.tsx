a0ba1543ea0f7ef0d8f141fa836e08ed0297860f
Erik Gurney <erik.gurney@hotmail.com>
2023-12-01 20:50:45 -0500

    fix(nav): set initial active link via useEffect due to hash

--- src/components/nav/NavLink.tsx
+++ src/components/nav/NavLink.tsx
@@ -9,11 +9,15 @@ type Props = {
 };
 
 function NavLink({ href, children }: Props) {
-    const [active, setActive] = useState(window ? window.location.hash === href : false);
+    const [active, setActive] = useState(false);
 
     const hashChange = useCallback(() => {
         setActive(window.location.hash === href);
-    }, []);
+    }, [href]);
+
+    useEffect(() => {
+        hashChange();
+    }, [href]);
 
     useEffect(() => {
         window.addEventListener("hashchange", hashChange);
@@ -21,11 +25,11 @@ function NavLink({ href, children }: Props) {
         return () => {
             window.removeEventListener("hashchange", hashChange);
         };
-    }, []);
+    }, [hashChange]);
 
     // Cannot use Nextjs <Link>, as Next uses history.push() for links and those are not hash change events.  A normal <a> tag must be used.
     return (
-        <a className={`text-lg py-2  transition-all duration-0 hover:duration-150 ${active ? "text-amber-200" : "text-white hover:text-amber-200"}`} href={href}>
+        <a className={`text-lg py-2  transition-all duration-0 hover:duration-150 ${active ? "text-amber-300" : "text-white hover:text-amber-300"}`} href={href}>
             {children}
         </a>
     );
