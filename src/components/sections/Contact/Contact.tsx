16f7bd4097e4c5b20c4a7849cc0d54097a10cfb9
Erik Gurney <erik.gurney@hotmail.com>
2023-12-18 13:45:33 -0500

    fix(contact): accomodate for localStorage not being available server-side

--- src/components/sections/Contact/Contact.tsx
+++ src/components/sections/Contact/Contact.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 // * Lib
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
@@ -14,10 +14,11 @@ import FormInput from "@/src/components/sections/Contact/FormInput";
 import { ContactFormSchema } from "@/src/utils/ContactFormSchema";
 import postContact from "@/src/actions/postContact";
 import useSectionObserver from "@/src/hooks/useSectionObserver";
+import Spinner from "../../Spinner";
 
 type Inputs = z.infer<typeof ContactFormSchema>;
 
-const submissionThresholdMinutes = 30;
+const submissionThresholdMinutes = 1;
 
 function Contact() {
     const { sectionRef } = useSectionObserver({ name: "contact" });
@@ -25,12 +26,29 @@ function Contact() {
     const [submitError, setSubmitError] = useState<string>();
     const [isLoading, setIsLoading] = useState(false);
 
-    const [lastSubmissionTime, setLastSubmissionTime] = useState(parseInt(localStorage.getItem("lastSubmissionTime") || "0"));
+    const [lastSubmissionTime, setLastSubmissionTime] = useState(0);
+    const [isInitializing, setIsInitializing] = useState(true);
     const currentTime = new Date().getTime();
     const canSubmit = currentTime - lastSubmissionTime >= submissionThresholdMinutes * 60 * 1000;
 
-    const [formAnimationClasses, setFormAnimationClasses] = useState(canSubmit ? "opacity-100 z-10" : "opacity-0");
-    const [messageAnimationClasses, setMessageAnimationClasses] = useState(canSubmit ? "opacity-0 -translate-y-2 -z-10" : "opacity-100 translate-y-0");
+    // Set intial last submission time
+    useEffect(() => {
+        setLastSubmissionTime(parseInt(localStorage.getItem("lastSubmissionTime") || "0"));
+    }, []);
+
+    // Set transition styles when canSubmit changes
+    useEffect(() => {
+        setFormAnimationClasses(canSubmit ? "opacity-100 z-10" : "opacity-0");
+        setMessageAnimationClasses(canSubmit ? "opacity-0 -translate-y-2 -z-10" : "opacity-100 translate-y-0");
+
+        if (isInitializing) {
+            setIsInitializing(false);
+        }
+    }, [isInitializing, canSubmit]);
+
+    // This state will hold the classes necessary to animate/transition the form and success message
+    const [formAnimationClasses, setFormAnimationClasses] = useState("opacity-0");
+    const [messageAnimationClasses, setMessageAnimationClasses] = useState("opacity-0");
 
     const {
         register,
@@ -65,10 +83,6 @@ function Contact() {
         const currentTime = new Date().getTime();
         setLastSubmissionTime(currentTime);
         localStorage.setItem("lastSubmissionTime", currentTime.toString());
-
-        // Animate form & success message transition
-        setMessageAnimationClasses("opacity-100 translate-y-0 z-10");
-        setFormAnimationClasses("opacity-0 -z-10");
     };
 
     return (
@@ -79,6 +93,12 @@ function Contact() {
                     <h2 className="text-3xl text-center text-amber-500 font-bold uppercase">Contact</h2>
                     <h2 className="font-inconsolata text-3xl sm:text-xl text-center text-slate-300 tracking-tighter">I&apos;d love to hear from you!</h2>
                 </div>
+
+                {isInitializing && (
+                    <div className="flex justify-center opacity-75 mt-10">
+                        <Spinner w="w-12" />
+                    </div>
+                )}
 
                 <div className="relative">
                     <form onSubmit={handleSubmit(submitForm)} className={`flex flex-col gap-12 transition-all duration-300 ease-in-out ${formAnimationClasses}`}>
--- src/components/sections/Contact/FormInput.tsx
+++ src/components/sections/Contact/FormInput.tsx
@@ -1,5 +1,5 @@
 // * Lib
-import { DetailedHTMLProps, HTMLInputTypeAttribute, InputHTMLAttributes } from "react";
+import { DetailedHTMLProps, HTMLInputTypeAttribute, InputHTMLAttributes, TextareaHTMLAttributes } from "react";
 import { FieldError, FieldErrors, FieldValues, UseFormRegisterReturn } from "react-hook-form";
 
 // * Components
@@ -21,7 +21,7 @@ function FormInput<T extends FieldValues>({ register, type = "text", name, label
 
     return (
         <div className="relative z-0">
-            {isTextarea ? <textarea {...register} id={name} className={twMerge(inputClassName, "min-h-[200px] max-h-[400px]")} placeholder="" /> : <input {...register} type={type} id={name} {...rest} className={inputClassName} placeholder="" />}
+            {isTextarea ? <textarea {...register} id={name} className={twMerge(inputClassName, "min-h-[200px] max-h-[400px]")} placeholder="" {...(rest as DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>)} /> : <input {...register} type={type} id={name} {...rest} className={inputClassName} placeholder="" />}
             <label htmlFor={name} className="absolute text-amber-200 peer-placeholder-shown:text-white peer-focus:text-amber-200 peer-placeholder-shown:opacity-60 duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:opacity-100 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-disabled:text-neutral-500">
                 {label}
             </label>
